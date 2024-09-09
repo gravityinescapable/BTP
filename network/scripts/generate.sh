2c968843814988c8edd060c253c4bbc0a1cdfcaa
Rishika <sharmarishika1412@gmail.com>
2024-09-09 11:46:19 +0530

    encryption details

--- network/scripts/generate.sh
+++ network/scripts/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate the crypto-config directory
-cryptogen generate --config=crypto-config.yaml
+cryptogen generate --config=../crytpo-config.yaml --output="../crypto-config"
 
 # Generate the channel configuration transaction
 configtxgen -profile TwoOrgsOrdererGenesis -outputBlock channel-artifacts/genesis.block
